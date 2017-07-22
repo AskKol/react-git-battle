248873bb7843098ceb055f1f340985148250e56f
Kola <kolapo.abolarinwa@slimmingworld.co.uk>
2017-07-22 16:12:43 +0100

    Begining to build navigational components
    Begining to build navigational components

--- app/components/App.js
+++ app/components/App.js
@@ -1,13 +1,18 @@
-var React = require('react');
-var Popular = require('./Popular');
+var React = require('react');//Es5
+var Popular = require('./Popular'); //Es5
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
 class App extends React.Component
 {
     render()
     {
         return (
-            <div className='container'>
-                <Popular />
-            </div>
+            <Router>
+                <div className='container'>
+                   <Route Path='/popular' component= {Popular} />
+                </div>
+            </Router>
         )
     }
 }
