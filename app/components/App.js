86390e878b26274a64740dc8b43825a36c5ebeeb
Kola <kolapo.abolarinwa@slimmingworld.co.uk>
2017-07-28 10:03:33 +0100

    Finished result
    Completed result component

--- app/components/App.js
+++ app/components/App.js
@@ -11,7 +11,7 @@ import
 import Nav from './Nav';
 import Home from './Home';
 import Battle from './Battle';
-
+import Results from './Results';
 
 class App extends React.Component
 {
@@ -25,6 +25,7 @@ class App extends React.Component
                     <Switch>
                         <Route exact path='/' component={Home} />
                         <Route exact path='/battle' component={Battle} />
+                        <Route exact path ='/battle/results' component={Results}/>
                         <Route path='/popular' component={Popular} />
                         <Route render={function ()
                         {
--- app/utils/Api.js
+++ app/utils/Api.js
@@ -1,5 +1,72 @@
 import axios from 'axios';
 
+const id = "YOUR_CLIENT_ID";
+const sec = "YOUR_SECRET_ID"
+const params = "?client_id=" + id + "&client_secret=" + sec;
+
+
+function  getProfile(username) {
+    return axios.get('https://api.github.com/users/' + username + params)
+        .then(function (aUser){
+            return aUser.data;
+        });
+}
+
+
+function getRepos(aUserName) {
+    return axios.get('https://api.github.com/users/' + aUserName + '/repos' + params + '&per_page=100');
+
+}
+
+function getStarcount(repos) {
+    return repos.data.reduce(function(count, repo) {
+        return count + repo.stargazers_count;
+    }, 0);
+}
+
+function calculateScore(profile, repos) {
+    let followers = profile.followers;
+    let totalStars = getStarcount(repos);
+
+    return (followers * 3) + totalStars;
+}
+
+function handleError(error) {
+    console.warn(error);
+    return null;
+}
+
+
+function getUserData(player) {
+    return axios.all([
+        getProfile(player),
+        getRepos(player)
+    ])
+        .then(function(data) {
+            let profile = data[0];
+            let repos = data[1];
+
+            return {
+                profile: profile,
+                score:calculateScore(profile,repos)
+            }
+        });
+}
+
+
+function sortPlayers(players) {
+    return players.sort(function(a, b) {
+        return b.score - a.score;
+    });
+}
+
+export function battle(players) {
+    return axios.all(
+        players.map(getUserData)
+    ).then(sortPlayers)
+        .catch(handleError)
+}
+
 export function fetchPopularRepos(language)
 {
     var encodedURI = window.encodeURI('https://api.github.com/search/repositories?q=stars:>1+language:'
